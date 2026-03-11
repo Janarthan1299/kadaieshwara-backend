4a34d440d6e419d41919b56e7f02250c10b9da88
nisanthg1010 <nisanthg1010@gmail.com>
2026-03-11 10:53:58 +0530

    Add new frontend URL to CORS

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://kadaieshwara-frontend-mbjx.vercel.app'
+    'https://kadaieshwara-frontend-mbjx.vercel.app',
+    'https://kadaieshwara.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
